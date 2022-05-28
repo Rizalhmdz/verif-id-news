e19a2da7c7e024ea167f5daafcdfa782d9da3574
Rizalhmdz <Rizalmhmmd.13zx@gmail.com>
2022-05-28 14:13:37 +0800

    test tanpa router

--- app/main.py
+++ app/main.py
@@ -1,47 +1,59 @@
-from flask import Flask, request, jsonify
+import re
+from flask import Flask, request, json
 from keras.preprocessing import text, sequence
 from pathlib import Path
 import tensorflow as tf
+import os
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-model = tf.keras.models.load_model(str(Path().absolute())+'\\api-v1')
+# model = tf.keras.models.load_model(str(Path().absolute())+'\\api-v1')
 
-@app.route('/')
-def home():
-    return 'Wellcome to verifIDNews'
+# model = tf.keras.models.load_model('D:\Project\Python\Data\\verifIDNews-api\\api-v1')
 
-@app.route('/predict' ,methods=['POST'])
-def predict():
-    if request.method == 'POST':
+# @app.route('/')
+# def home():
+#     return 'Wellcome to verifIDNews'
+
+# @app.route('/predict' ,methods=['POST'])
+def predict(user_input):
+    # user_input = [request.form.get('input')]
+    # model = keras.models.load_model(str(os.path.dirname(__file__))+ '/api_model.h5')
+    # model = pickle.load(open(str(os.path.dirname(__file__))+ '\\api-v1', 'rb'))
+    path = str(os.path.dirname(__file__)) + '\\..\\api-v1'
+    model= tf.keras.models.load_model(path )
+    max_features = 1000
+    maxlen = 300
         
-        # model = tf.keras.models.load_model('D:\Project\Python\Data\\verifIDNews-api\\api-v1')
-        user_input = [request.form.get('input')]
+    tokenizer = text.Tokenizer(num_words = max_features)
+    tokenized_test = tokenizer.texts_to_sequences(user_input)
+    user_input_tokenized = sequence.pad_sequences(tokenized_test, maxlen = maxlen)
+    output = model.predict(user_input_tokenized)
         
-        max_features = 10000
-        maxlen = 300
+    label = ''
         
-        tokenizer = text.Tokenizer(num_words = max_features)
-        tokenized_test = tokenizer.texts_to_sequences(user_input)
-        user_input_tokenized = sequence.pad_sequences(tokenized_test, maxlen = maxlen)
-        output = model.predict(user_input_tokenized)
+    # if output > 0.75 : 
+    #     label = 'Berita Terindikasi sebagai Fakta'
+    # elif output <= 0.75 and output > 0.60 : 
+    #     label = 'Berita Terindikasi lemah Fakta'
+    # elif output <= 0.6 and output > 0.5 :
+    #     label = 'Berita perlu ditinjau ulang antara Fakta atau Hoax'
+    # elif output <= 0.5 and output > 0.35 : 
+    #     label = 'Berita Terindikasi Lemah sebagai Hoax'
+    # elif output <= 0.35 and output > 0.15 : 
+    #     label = 'Berita Terindikasi sebagai Hoax'
+    # elif output <= 0.15 :
+    #     label = 'Berita Terindikasi Kuat sebagai Hoax'
         
-        label = ''
-        
-        if output > 0.75 : 
-            label = 'Berita Terindikasi sebagai Fakta'
-        elif output <= 0.75 and output > 0.60 : 
-            label = 'Berita Terindikasi lemah Fakta'
-        elif output <= 0.6 and output > 0.5 :
-            label = 'Berita perlu ditinjau ulang antara Fakta atau Hoax'
-        elif output <= 0.5 and output > 0.35 : 
-            label = 'Berita Terindikasi Lemah sebagai Hoax'
-        elif output <= 0.35 and output > 0.15 : 
-            label = 'Berita Terindikasi sebagai Hoax'
-        elif output <= 0.15 :
-            label = 'Berita Terindikasi Kuat sebagai Hoax'
-    
-        
-    return jsonify({'indicator': str(user_input),
-                    'msg' : label
-                    })
+    # return bytes(output)
+    # response = app.response_class(
+    #     response= json.dumps({'indicator': str(output),'msg' : user_input }),
+    #     status=200,
+    #     mimetype='application/json'
+    # )
+
+    return output
+
+
+
+print(predict('jij'))
